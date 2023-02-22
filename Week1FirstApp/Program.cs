18702723e5901b95e006a8eb0ab4a9d5fa615193
yavuz06 <yavuzselmkaya@gmail.com>
2023-02-22 18:47:24 +0300

    add my first app

---
+++ Week1FirstApp/Program.cs
@@ -0,0 +1,7 @@
+﻿//Yavuz Selim Kaya
+
+string myname = "Yavuz";
+string mylastname = "Kaya";
+    
+//Console.WriteLine("Hello" + " " + myname);
+Console.WriteLine($"Hello, {myname} {mylastname} !");
